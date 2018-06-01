d89c9954d92a1d99851c67bc40d7108a74b8d8bf
Jordan  Haddadi <jordanhaddadi@gmail.com>
2018-06-01 20:24:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function currentLine(katzDeli) {
     text += "The line is currently:" + i;
     i++;
 }
-  if (i) {
+  if (katzDeli.length === 0) {
     return `The line is currently empty.` }
   else (i); {
     return `The line is currently:` + i;
