7acad1fff1e0be708d9445c9efd5c581bcda04f1
Jordan  Haddadi <jordanhaddadi@gmail.com>
2018-06-01 20:27:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function nowServing(katzDeli) {
 var line = [];
 
 function currentLine(katzDeli) {
-  let i = 0;
+  var i = 0;
   while (i < katzDeli.length) {
     line.push (` `+[i+1]+`.`+katzDeli[i]);
     text += "The line is currently:" + i;
