b71c4dc8d54d5b0489330b27cec9f5d996a5587e
Jordan  Haddadi <jordanhaddadi@gmail.com>
2018-06-01 20:31:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,8 +20,7 @@ var line = [];
 function currentLine(katzDeli) {
   let i = 0;
   while (i < katzDeli.length) {
-    line.push ([i+1]+katzDeli[i]);
-    text += "The line is currently:" + i;
+    line.push (` `+[i+1]+`. ` + katzDeli[i]);
     i++;
 }
   if (katzDeli.length === 0) {
@@ -30,14 +29,3 @@ function currentLine(katzDeli) {
     return `The line is currently:` + line;
   }
 }
-//   function currentLine(katzDeli) {
-// +  let i = 0;
-// +  while (i < katzDeli.length) {
-// +    line.push(` `+[i+1]+`. `  + katzDeli[i])
-// +    i++;
-// +  }
-// +  if (katzDeli.length === 0) {
-// +    return "The line is currently empty.";
-// +  } else
-// +  return(`The line is currently:` + line);
-// +}
